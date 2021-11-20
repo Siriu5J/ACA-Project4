2e7de2715ac6abd3e39652d0ea1e89d4ae486293
JackWilliamson116 <jaydubbs3@gmail.com>
2021-11-19 19:56:52 -0500

    Baseline issueUnit implementation implementing first try at loadbuffer issue as well as some outlined code for the alu and mult units. Also implemented a createIssuedInst() function.

--- src/tomasulogui/IssueUnit.java
+++ src/tomasulogui/IssueUnit.java
@@ -7,12 +7,60 @@ public class IssueUnit {
     PipelineSimulator simulator;
     IssuedInst issuee;
     Object fu;
+    EXEC_TYPE instType;
+    int pc;
 
     public IssueUnit(PipelineSimulator sim) {
       simulator = sim;
     }
+    
+    public void createIssuedInst() {
+        Instruction i = simulator.memory.getInstAtAddr(pc);
+        issuee = IssuedInst.createIssuedInst(i);
+        pc+=4;
+        //Need to parse as much as posssible for this issuedInst
+    }
 
     public void execCycle() {
+        createIssuedInst();
+        switch(instType){
+            case LOAD:
+                if(simulator.loader.isReservationStationAvail()&&!simulator.reorder.isFull()){
+                    simulator.reorder.updateInstForIssue(issuee);
+                    simulator.loader.acceptIssue(issuee);
+                }
+                else{
+                    pc-=4;
+                }
+                break;
+            case ALU:
+                /*
+                if(simulator.alu.isReservationStationAvail()&&!simulator.reorder.isFull()){
+                    simulator.reorder.updateInstForIssue(issuee);
+                    simulator.alu.acceptIssue(issuee);
+                }
+                */
+                break;
+            case MULT:
+                /*
+                if(simulator.mult.isReservationStationAvail()&&!simulator.reorder.isFull()){
+                    simulator.reorder.updateInstForIssue(issuee);
+                    simulator.mult.acceptIssue(issuee);
+                }
+                */
+                break;
+            case DIV:
+                break;
+            case BRANCH:
+                /*
+                BRANCH IS FOR NOT 11/19/21
+                */
+                break;
+            case NONE:
+                break;
+            default:
+                break;
+        }
       // an execution cycle involves:
       // 1. checking if ROB and Reservation Station avail
       // 2. issuing to reservation station, if no structural hazard
