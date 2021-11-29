8169068b44ce59110cccbaaf69a7c3aaaabf8ef1
JackWilliamson116 <jaydubbs3@gmail.com>
2021-11-28 22:10:08 -0500

    50% done with ROBEntry

--- src/tomasulogui/ROBEntry.java
+++ src/tomasulogui/ROBEntry.java
@@ -8,14 +8,20 @@ public class ROBEntry {
     boolean predictTaken = false;
     boolean mispredicted = false;
     int instPC = -1;
+    // Destination register = write Reg
     int writeReg = -1;
+    // The value to be written to the write Reg
     int writeValue = -1;
+    // This is the tag that the ROB is snooping for on the cdb
+    int tag = -1;
 
     IssuedInst.INST_TYPE opcode;
     IssuedInst.INST_RIJ instType;
     boolean executing = false;
     boolean complete = false;
     boolean shouldWb = false;
+    
+    
 
     public ROBEntry(ReorderBuffer buffer) {
         rob = buffer;
@@ -89,12 +95,40 @@ public class ROBEntry {
         // TODO - This is a long and complicated method, probably the most complex
         // of the project.  It does 2 things:
         // 1. update the instruction, as shown in 2nd line of code above
+        
+
+        // Figure out if any of the registers are valid or not
+        // Tag the source registers that are not valid
+        
+        inst.setRegDestTag(frontQ);
+        rob.setTagForReg(inst.getRegDest(), frontQ);
+        tag = frontQ;
+        if(inst.regSrc1Used){
+            if(rob.getTagForReg(inst.getRegSrc1())==-1){
+                inst.setRegSrc1Valid();
+                inst.setRegSrc1Value(rob.getDataForReg(inst.getRegSrc1()));
+            }
+            else{
+                inst.setRegSrc1Tag(rob.getTagForReg(inst.getRegSrc1()));
+            }
+        }
+        if(inst.regSrc2Used){
+            if(rob.getTagForReg(inst.getRegSrc2())==-1){
+                inst.setRegSrc2Valid();
+                inst.setRegSrc2Value(rob.getDataForReg(inst.getRegSrc2()));
+            }  
+            else{
+                inst.setRegSrc2Tag(rob.getTagForReg(inst.getRegSrc2()));
+            }
+        }
+        // 2. update the fields of the ROBEntry, as shown in the 1st line of code above
         instPC = inst.getPC();
         writeReg = inst.getRegDest();
         instType = inst.getRIJType();
-        // 2. update the fields of the ROBEntry, as shown in the 1st line of code above
-        inst.setRegDestTag(frontQ);
-
+        shouldWb = false;
+        if(!inst.isBranch() && !(inst.opcode == IssuedInst.INST_TYPE.STORE)){
+            shouldWb = true;
+        }
     }
 
 }
