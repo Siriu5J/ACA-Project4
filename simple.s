4f0ab6e9df5d6a0f2ae4145d7c2d0de2b15a3f13
Samuel Jiang <samueljiang@outlook.com>
2021-12-01 19:50:38 -0500

    Added Dr. G's example

---
+++ simple.s
@@ -0,0 +1,23 @@
+Begin Assembly
+ADDI R1, R0, 4000
+ADDI R2, R0, 4004
+ADDI R10, R0, 12
+Label jump
+LW R3, 0(R1)
+LW R4, 0(R2)
+ADD R5, R3, R4
+SW R5, 4(R2)
+BLEZ R6, jump
+ADD R1, R3, R2
+HALT
+
+End Assembly
+-- begin main data
+Begin Data 4000 44
+6
+3
+0
+End Data
+-- stack
+Begin Data 5000 100
+End Data
